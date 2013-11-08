23336befccfe27a8b49ea70f01b5f0c0a07d7c95
shireen <shireen@shadowhealth.com>
2013-11-08 13:20:41 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DBTool::Application.config.secret_token = '43b58418323560b7dc24dfe2576b815f6b2b3bd7b586e6abe517161cf0814fb72937280c78b76312f7300a8074982b4d74b352fd165566e85d9abde529d8f60c'
